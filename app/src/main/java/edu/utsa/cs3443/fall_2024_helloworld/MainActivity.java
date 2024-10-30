0507bd3033f93c1c841c1e62477b76052bacaf31
itswheeler <swheeler911@me.com>
2024-10-30 09:07:53 -0500

    Wheeler - UI update Created placeholder MainActivity Update MainActivity.java to create 4 buttons Setup place holder background and added clippy icon.

--- app/src/main/java/edu/utsa/cs3443/fall_2024_helloworld/MainActivity.java
+++ app/src/main/java/edu/utsa/cs3443/fall_2024_helloworld/MainActivity.java
@@ -1,6 +1,10 @@
 package edu.utsa.cs3443.fall_2024_helloworld;
 
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button newBtn;
+    String[] calNames = {"Mortgage Calculator", "Auto Loan Calculator", "Interest Calculator", "Recent Calculations"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        for(String cal : calNames){
+            addButton(cal);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    private void addButton(String buttonName) {
+        LinearLayout layout = findViewById(R.id.buttonLayout);
+        int style = R.style.button;
+
+        newBtn = new Button(new ContextThemeWrapper(this, style), null, style);
+        newBtn.setText(buttonName);
+        newBtn.setHeight(260);
+        newBtn.setTag(buttonName);
+        newBtn.setOnClickListener(this);
+
+        layout.addView(newBtn);
     }
 }
