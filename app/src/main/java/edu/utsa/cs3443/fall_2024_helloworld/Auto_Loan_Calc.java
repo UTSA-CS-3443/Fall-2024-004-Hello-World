0220b7e700d7750625ac6ab414a9396fa8c874ff
itswheeler <swheeler911@me.com>
2024-10-31 17:17:13 -0500

    Wheeler - UI update Added 3 activities for the 3 calculators and a temporary back button

---
+++ app/src/main/java/edu/utsa/cs3443/fall_2024_helloworld/Auto_Loan_Calc.java
@@ -0,0 +1,39 @@
+package edu.utsa.cs3443.fall_2024_helloworld;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Auto_Loan_Calc extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_auto_loan_calc);
+        setUpButton(R.id.backbutton);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.backbutton){
+            Intent intent = new Intent(this, MainActivity.class);
+            //intent.putExtra(key,passedValue);
+            startActivity(intent);
+        }
+
+    }
+
+    private void setUpButton(int buttonID){
+        Button button = findViewById(buttonID);
+        button.setOnClickListener(this);
+    }
+}
