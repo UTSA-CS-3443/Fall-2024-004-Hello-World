d56936e0c79c5db48f0b5f3c3960fcfc2c931852
Carlos Martinez <guitardude9642@gmail.com>
2024-10-31 17:12:16 -0500

    fixed comment typos

--- app/src/main/java/edu/utsa/cs3443/fall_2024_helloworld/MainCalc.java
+++ app/src/main/java/edu/utsa/cs3443/fall_2024_helloworld/MainCalc.java
@@ -3,7 +3,7 @@ package edu.utsa.cs3443.fall_2024_helloworld;
 /** This class is meant to server as a foundation class where other calculator class
  * will extend from.
  *
- * Authors: Collaborative Effort of the team
+ * Authors: Collaborative effort of the team
  */
 public class MainCalc {
     /**
