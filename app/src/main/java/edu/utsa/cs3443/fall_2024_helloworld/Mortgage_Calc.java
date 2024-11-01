49224a062bb4d83d224346a4b3c40479e56758b2
itswheeler <swheeler911@me.com>
2024-11-01 08:42:14 -0500

    Wheeler - UI update Created 3 fields on the mortgage calculator screen and configured a submit button that is disabled until the textEdit fields are filled out. When the submit button is pressed the data in the text fields is assigned to variables.

--- app/src/main/java/edu/utsa/cs3443/fall_2024_helloworld/Mortgage_Calc.java
+++ app/src/main/java/edu/utsa/cs3443/fall_2024_helloworld/Mortgage_Calc.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,12 +12,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Mortgage_Calc extends AppCompatActivity implements View.OnClickListener {
 
-
+    int[] fields = {R.id.loanAmount,R.id.loanApr,R.id.loanYears};
+    float loanAmount;
+    float loanAPR;
+    int loanYears;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mortgage_calc);
         setUpButton(R.id.backbutton);
+        setUpButton(R.id.submit);
+
+
+
     }
 
 
@@ -28,14 +36,40 @@ public class Mortgage_Calc extends AppCompatActivity implements View.OnClickList
 
         if(v.getId() == R.id.backbutton){
             Intent intent = new Intent(this, MainActivity.class);
-            //intent.putExtra(key,passedValue);
             startActivity(intent);
         }
+        if(!getTextEdit(fields[0]).equalsIgnoreCase("") && !getTextEdit(fields[1]).equalsIgnoreCase("") && !getTextEdit(fields[2]).equalsIgnoreCase("")){
+            enableButton(R.id.submit);
+
+        } else {
+            disableButton(R.id.submit);
+        }
+        if(v.getId() == R.id.submit){
+            loanAmount = Float.parseFloat(getTextEdit(fields[0]));
+            loanAPR = Float.parseFloat(getTextEdit(fields[1]));
+            loanYears = Integer.parseInt(getTextEdit(fields[2]));
+
+        }
+
+
 
     }
 
     private void setUpButton(int buttonID){
         Button button = findViewById(buttonID);
         button.setOnClickListener(this);
+    }
+    private void disableButton(int buttonID){
+        Button button = findViewById(buttonID);
+        button.setEnabled(false);
+    }
+    private void enableButton(int buttonID){
+        Button button = findViewById(buttonID);
+        button.setEnabled(true);
+    }
+
+    private String getTextEdit(int editID){
+        EditText mEdit = findViewById(editID);
+        return mEdit.getEditableText().toString();
     }
 }
