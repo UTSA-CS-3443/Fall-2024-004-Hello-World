3fac13e3f6bb06be011f9056b56b09ca0bc1b73e
Joseph <120499523+idontknowkarate@users.noreply.github.com>
2024-11-15 13:31:59 -0600

    Joseph - Created the AutoLoanCalculation and InterestCalculation classes.

--- app/src/main/java/edu/utsa/cs3443/fall_2024_helloworld/Model/InterestCalculation.java
+++ app/src/main/java/edu/utsa/cs3443/fall_2024_helloworld/Model/InterestCalculation.java
@@ -4,4 +4,32 @@ import java.io.Serializable;
 
 
 public class InterestCalculation extends Calculation implements Serializable {
+    private double principal;
+    private double interestRate;
+    private int timesCompoundedPerYear;
+    private double years;
+    private double compoundInterest;
+    private double continuousCompoundInterest;
+
+    public InterestCalculation(double principal, double interestRate, int timesCompoundedPerYear, double years) {
+        this.principal = principal;
+        this.interestRate = interestRate;
+        this.timesCompoundedPerYear = timesCompoundedPerYear;
+        this.years = years;
+
+        calculateInterest();
+    }
+
+    public double getCompoundInterest() {
+        return compoundInterest;
+    }
+
+    public double getContinuousCompoundInterest() {
+        return continuousCompoundInterest;
+    }
+
+    public void calculateInterest() {
+        this.compoundInterest = calcCompoundInterest(principal, interestRate, timesCompoundedPerYear, years);
+        this.continuousCompoundInterest = calcContinuousCompoundInterest(principal, interestRate, years);
+    }
 }
